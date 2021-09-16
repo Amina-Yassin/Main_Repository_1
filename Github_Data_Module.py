24feb2104a130670041f017351bc568dab27a943
Amina-Yassin <90866877+Amina-Yassin@users.noreply.github.com>
2021-09-16 13:16:49 -0600

    Add files via upload

---
+++ Github_Data_Module.py
@@ -0,0 +1,7 @@
+# print "Hello World"
+"""AMINA'S Database for Github_Data_Module.py"""
+x_1 = 5
+x_2 = 10
+x_3 = 15
+x_4 = 20
+x_5 = 25
---
+++ Main_File_for_Github.py
@@ -0,0 +1,20 @@
+#Main Program for Github Data Module
+print()
+print ("Welcome to the module import program.")
+
+import Github_Data_Module
+
+x_1 = Github_Data_Module.x_1
+x_2 = Github_Data_Module.x_2
+x_3 = Github_Data_Module.x_3
+x_4 = Github_Data_Module.x_4
+x_5 = Github_Data_Module.x_5
+
+print ("We shall now access the previous module's data.")
+print (x_1, "*", x_2, "=", x_1 * x_2)
+print (x_3, "*", x_4, "*", x_5, "=", x_3 * x_4 * x_5)
+
+print()
+print("""The tansfer is complete. 
+Thank you for your time.
+""")
